ce7f9b7f1f18e934954a831aac24c512f4f427fe
LAGNEA <zuiriya@gmail.com>
2020-10-23 20:45:56 +0900

    모의전 랭크 추가

---
+++ javascript/rank_options.js
@@ -0,0 +1,12 @@
+var rank_Options = [
+    { "rank_Text" : "모의전 랭크를 선택하세요", "rank_Value" : "NULL" },
+    { "rank_Text" : "D등급", "rank_Value" : "rank_01" },
+    { "rank_Text" : "C등급", "rank_Value" : "rank_02" },
+    { "rank_Text" : "B등급", "rank_Value" : "rank_03" },
+    { "rank_Text" : "A등급", "rank_Value" : "rank_04" },
+    { "rank_Text" : "S등급", "rank_Value" : "rank_05" },
+    { "rank_Text" : "SS등급", "rank_Value" : "rank_06" },
+    { "rank_Text" : "SSS등급", "rank_Value" : "rank_07" },
+    { "rank_Text" : "X등급", "rank_Value" : "rank_08" }
+]
+  
